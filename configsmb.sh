17af9b7adeaac44e2534fa2f70dfae6a3adeb9cb
Sandro Silveira <sandrosilveira@gmail.com>
2020-11-19 16:47:50 -0300

    Update configsmb.sh

--- configsmb.sh
+++ configsmb.sh
@@ -30,35 +30,35 @@ fi
 # Configurações globais sugeridas pela Rech
 if [ -f /etc/debian_version ]; then
    if [ -z "$(grep -i "follow symlinks" /etc/samba/smb.conf)" ]; then
-      sudo sed -i '/\[global\]/a         follow symlinks = yes' /etc/samba/smb.conf
+      sed -i '/\[global\]/a         follow symlinks = yes' /etc/samba/smb.conf
    fi
    if [ -z "$(grep -i "wide symlinks" /etc/samba/smb.conf)" ]; then
-      sudo sed -i '/\[global\]/a         wide symlinks = yes' /etc/samba/smb.conf
+      sed -i '/\[global\]/a         wide symlinks = yes' /etc/samba/smb.conf
    fi
    if [ -z "$(grep -i "unix extensions" /etc/samba/smb.conf)" ]; then
-      sudo sed -i '/\[global\]/a         unix extensions = no' /etc/samba/smb.conf
+      sed -i '/\[global\]/a         unix extensions = no' /etc/samba/smb.conf
    fi
 fi
 if [ -z "$(grep -i "map to guest" /etc/samba/smb.conf)" ]; then
-   sudo sed -i '/\[global\]/a         map to guest = bad user' /etc/samba/smb.conf
+   sed -i '/\[global\]/a         map to guest = bad user' /etc/samba/smb.conf
 fi
 if [ -z "$(grep -i "usershare allow" /etc/samba/smb.conf)" ]; then
-   sudo sed -i '/\[global\]/a         usershare allow guests = yes' /etc/samba/smb.conf
+   sed -i '/\[global\]/a         usershare allow guests = yes' /etc/samba/smb.conf
 fi
 if [ -z "$(grep -i "locking = yes" /etc/samba/smb.conf)" ]; then
-   sudo sed -i '/\[global\]/a         locking = yes' /etc/samba/smb.conf
+   sed -i '/\[global\]/a         locking = yes' /etc/samba/smb.conf
 fi
 if [ -z "$(grep -i "kernel oplocks" /etc/samba/smb.conf)" ]; then
-   sudo sed -i '/\[global\]/a         kernel oplocks = yes' /etc/samba/smb.conf
+   sed -i '/\[global\]/a         kernel oplocks = yes' /etc/samba/smb.conf
 fi
 if [ -z "$(grep -i "security = user" /etc/samba/smb.conf)" ]; then
-   sudo sed -i '/\[global\]/a         security = user' /etc/samba/smb.conf
+   sed -i '/\[global\]/a         security = user' /etc/samba/smb.conf
 fi
 if [ -z "$(grep -i "min protocol = SMB2" /etc/samba/smb.conf)" ]; then
-   sudo sed -i '/\[global\]/a         min protocol = SMB2' /etc/samba/smb.conf
+   sed -i '/\[global\]/a         min protocol = SMB2' /etc/samba/smb.conf
 fi
 if [ -z "$(grep -i "socket options" /etc/samba/smb.conf)" ]; then
-   sudo sed -i '/\[global\]/a         socket options = TCP_NODELAY SO_RCVBUF=8192 SO_SNDBUF=8192' /etc/samba/smb.conf
+   sed -i '/\[global\]/a         socket options = TCP_NODELAY SO_RCVBUF=8192 SO_SNDBUF=8192' /etc/samba/smb.conf
 fi
 
 # Se ainda não tem o compartilhamento do SIGER configurado
